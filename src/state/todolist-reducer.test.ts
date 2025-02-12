b06707acdb7e21425438eeb040e4c597c3979ce9
bloodyrainday <bloodyrainday@gmail.com>
2025-02-12 22:06:16 +0300

    create test 'add todolist'

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -1,5 +1,9 @@
 import { expect, test } from "vitest";
-import { RemoveTodolistAC, todolistReducer } from "./todolist-reducer";
+import {
+  AddTodolistAC,
+  RemoveTodolistAC,
+  todolistReducer,
+} from "./todolist-reducer";
 import { TodolistType } from "../App";
 
 test("remove todolist which id was provided", () => {
@@ -20,4 +24,26 @@ test("remove todolist which id was provided", () => {
   const endState = todolistReducer(startState, action);
 
   expect(endState.length).toBe(1);
+  expect(endState[0].id).toBe("1");
+});
+
+test("add todolist which title was provided", () => {
+  const startState: TodolistType[] = [
+    {
+      id: "1",
+      title: "what to learn",
+      filter: "all",
+    },
+    {
+      id: "2",
+      title: "what to buy",
+      filter: "all",
+    },
+  ];
+
+  const action = AddTodolistAC("what to eat");
+  const endState = todolistReducer(startState, action);
+
+  expect(endState.length).toBe(3);
+  expect(endState[0].title).toBe("what to eat");
 });
--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,22 +1,37 @@
+import { v1 } from "uuid";
 import { TodolistType } from "../App";
 
 export type RemoveTodolistActionType = {
-  type: string;
+  type: "REMOVE-TODOLIST";
   id: string;
 };
 
+export type AddTodolistActionType = {
+  type: "ADD-TODOLIST";
+  title: string;
+  id: string;
+};
+
+export type ActionsType = RemoveTodolistActionType | AddTodolistActionType;
+
 export const todolistReducer = (
   state: TodolistType[],
-  action: RemoveTodolistActionType
-) => {
+  action: ActionsType
+): TodolistType[] => {
   switch (action.type) {
     case "REMOVE-TODOLIST":
       return state.filter((s) => s.id !== action.id);
+    case "ADD-TODOLIST":
+      return [{ id: action.id, title: action.title, filter: "all" }, ...state];
     default:
       throw new Error("I dont understand this action type");
   }
 };
 
-export const RemoveTodolistAC = (id: string) => {
+export const RemoveTodolistAC = (id: string): RemoveTodolistActionType => {
   return { type: "REMOVE-TODOLIST", id };
+};
+
+export const AddTodolistAC = (title: string): AddTodolistActionType => {
+  return { type: "ADD-TODOLIST", title, id: v1() };
 };
