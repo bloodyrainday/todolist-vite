3279e033499790173a6b5e6bd098ef76c797ac14
bloodyrainday <bloodyrainday@gmail.com>
2025-02-13 18:14:23 +0300

    add task-reducer and test for it

---
+++ src/state/task-reducer..ts
@@ -0,0 +1,28 @@
+import { v1 } from "uuid";
+import { FilterType, TaskStorageType, TodolistType } from "../App";
+
+export type RemoveTaskActionType = {
+  type: "REMOVE-TASK";
+  todolistId: string;
+  taskId: string;
+};
+
+export const taskReducer = (
+  state: TaskStorageType,
+  action: RemoveTaskActionType
+): TaskStorageType => {
+  switch (action.type) {
+    case "REMOVE-TASK":
+      return state;
+
+    default:
+      throw new Error("I dont understand this action type");
+  }
+};
+
+export const RemoveTasktAC = (
+  todolistId: string,
+  taskId: string
+): RemoveTaskActionType => {
+  return { type: "REMOVE-TASK", todolistId, taskId };
+};
---
+++ src/state/task-reducer.test.ts
@@ -0,0 +1,51 @@
+import { expect, test } from "vitest";
+import { TaskStorageType } from "../App";
+import { v1 } from "uuid";
+import { RemoveTasktAC, taskReducer } from "./task-reducer.";
+
+test("remove todolist which id was provided", () => {
+  const todolistId1 = v1();
+  const todolistId2 = v1();
+  const startState: TaskStorageType = {
+    [todolistId1]: [
+      {
+        id: v1(),
+        title: "HTML&CSS",
+        isDone: true,
+      },
+      {
+        id: v1(),
+        title: "JS",
+        isDone: false,
+      },
+      {
+        id: v1(),
+        title: "React",
+        isDone: true,
+      },
+    ],
+    [todolistId2]: [
+      {
+        id: v1(),
+        title: "milk",
+        isDone: false,
+      },
+      {
+        id: v1(),
+        title: "meat",
+        isDone: false,
+      },
+      {
+        id: v1(),
+        title: "bread",
+        isDone: true,
+      },
+    ],
+  };
+
+  const action = RemoveTasktAC(todolistId2, "2");
+  const endState = taskReducer(startState, action);
+
+  expect(endState[todolistId2].length).toBe(2);
+  expect(endState[todolistId1].length).toBe(3);
+});
