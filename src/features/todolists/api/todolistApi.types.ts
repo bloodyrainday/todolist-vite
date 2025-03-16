2de6031055e7110035e4fc3ad369bfde29e5462f
bloodyrainday <bloodyrainday@gmail.com>
2025-03-16 17:22:38 +0300

    add Task and TaskItem types

--- src/features/todolists/api/todolistApi.types.ts
+++ src/features/todolists/api/todolistApi.types.ts
@@ -4,3 +4,23 @@ export type Todolist = {
   addedDate: string;
   order: number;
 };
+
+export type Task = {
+  error: string;
+  totalCount: number;
+  items: TaskItem[];
+};
+
+export type TaskItem = {
+  description: string;
+  title: string;
+  completed: boolean;
+  status: number;
+  priority: number;
+  startDate: string;
+  deadline: string;
+  id: string;
+  todoListId: string;
+  order: number;
+  addedDate: string;
+};
