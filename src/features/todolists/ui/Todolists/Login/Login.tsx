4b846b4e593054b48d9a16eca975b0e0e049c19a
bloodyrainday <bloodyrainday@gmail.com>
2025-04-16 23:25:56 +0300

    addloginSchema file

--- src/features/todolists/ui/Todolists/Login/Login.tsx
+++ src/features/todolists/ui/Todolists/Login/Login.tsx
@@ -8,7 +8,9 @@ import FormGroup from "@mui/material/FormGroup"
 import FormLabel from "@mui/material/FormLabel"
 import Grid from "@mui/material/Grid2"
 import TextField from "@mui/material/TextField"
-import { SubmitHandler, useForm } from "react-hook-form"
+import { spawn } from "child_process"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import styles from "./Login.module.css"
 
 type Inputs = {
   email: string
@@ -24,12 +26,14 @@ export const Login = () => {
     control,
     formState: { errors },
   } = useForm<Inputs>({ defaultValues: { email: "", password: "", rememberMe: false } })
+  console.log(errors)
 
   const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
+    reset()
     console.log(data)
   }
   return (
@@ -57,9 +61,30 @@ export const Login = () => {
         </FormLabel>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FormGroup>
-            <TextField label="Email" margin="normal" {...register("email")} />
+            <TextField
+              label="Email"
+              margin="normal"
+              {...register("email", {
+                required: "email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Incorrect email address",
+                },
+              })}
+            />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
             <TextField type="password" label="Password" margin="normal" {...register("password")} />
-            <FormControlLabel label="Remember me" control={<Checkbox />} {...register("rememberMe")} />
+            <FormControlLabel
+              label="Remember me"
+              control={
+                <Controller
+                  name={"rememberMe"}
+                  control={control}
+                  render={({ field: { value, ...rest } }) => <Checkbox {...rest} checked={value} />}
+                />
+              }
+              {...register("rememberMe")}
+            />
             <Button type="submit" variant="contained" color="primary">
               Login
             </Button>
