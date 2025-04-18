0e50755c9164fcfa0ec28b1562f6a37b0076f62d
bloodyrainday <bloodyrainday@gmail.com>
2025-04-18 10:30:23 +0300

    add password validation

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -1,8 +1,11 @@
 import { z } from "zod"
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().min(1, { message: "email is required" }).email({ message: "email is invalid" }),
+  password: z
+    .string()
+    .min(1, { message: "password is required" })
+    .min(3, { message: "password must be at least 3 characters long" }),
   rememberMe: z.boolean(),
 })
 
