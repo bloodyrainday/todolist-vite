ea7e57439bc74387035098494ed2a98a9229ba0f
bloodyrainday <bloodyrainday@gmail.com>
2025-04-18 10:20:29 +0300

    add link-button in the PageNotFound component

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,13 @@
+import Button from "@mui/material/Button"
 import styles from "./PageNotFound.module.css"
+import { Link } from "react-router"
 
 export const PageNotFound = () => (
-  <>
+  <div className={styles.wrapper}>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
-  </>
+    <Button className={styles.link} variant="contained" component={Link} to="/">
+      get back to the main page
+    </Button>
+  </div>
 )
