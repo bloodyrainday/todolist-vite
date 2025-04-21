74e54d50d45a091f192681a850ba2b374460fa20
bloodyrainday <bloodyrainday@gmail.com>
2025-04-21 15:13:45 +0300

    add auth-slice

---
+++ src/features/auth/model/auth-slice.ts
@@ -0,0 +1,25 @@
+import { createAppSlice } from "@/common/utils"
+import { Inputs } from "../lib/schemas"
+
+export const authSlice = createAppSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  reducers: (create) => ({
+    loginTC: create.asyncThunk(
+      async (data: Inputs, { dispatch, rejectWithValue }) => {
+        // логика санки для авторизации
+      },
+      {
+        fulfilled: (state, action) => {
+          state.isLoggedIn = action.payload.isLoggedIn
+        },
+      },
+    ),
+  }),
+})
+
+export const { selectIsLoggedIn } = authSlice.selectors
+export const { loginTC } = authSlice.actions
+export const authReducer = authSlice.reducer
