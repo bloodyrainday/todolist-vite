024a7e250574428714839f5477fb212c4a4e1d0c
bloodyrainday <bloodyrainday@gmail.com>
2025-05-08 19:00:50 +0300

    add PARSING_ERROR catching

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,17 +1,26 @@
 import { AUTH_TOKEN } from "@/common/constants"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { setError } from "./app-slice"
 
 export const baseApi = createApi({
   reducerPath: "todolistApi",
   tagTypes: ["Todolist", "Task"],
   baseQuery: async (args, api, extraOptions) => {
-    return fetchBaseQuery({
+    const result = await fetchBaseQuery({
       baseUrl: import.meta.env.VITE_BASE_URL,
       prepareHeaders: (headers) => {
         headers.set("API-KEY", import.meta.env.VITE_API_KEY)
         headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
       },
     })(args, api, extraOptions)
+
+    if (result.error) {
+      if (result.error.status === "FETCH_ERROR") {
+        api.dispatch(setError({ error: result.error.error }))
+      }
+    }
+
+    return result
   },
   endpoints: () => ({}),
 })
