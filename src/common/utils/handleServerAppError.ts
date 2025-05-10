90a17a4665b969a0d3b0d560c4a2a2e6a396305d
bloodyrainday <bloodyrainday@gmail.com>
2025-05-10 23:23:41 +0300

    comment handleServerAppError and handleServerNetworkError files

--- src/common/utils/handleServerAppError.ts
+++ src/common/utils/handleServerAppError.ts
@@ -1,16 +1,16 @@
-import { setError, setStatus } from "@/app/app-slice"
-import type { BaseResponse } from "@/common/types"
-import type { Dispatch } from "@reduxjs/toolkit"
+// import { setError, setStatus } from "@/app/app-slice"
+// import type { BaseResponse } from "@/common/types"
+// import type { Dispatch } from "@reduxjs/toolkit"
 
-// BaseResponse<{
-//     item: Task;
-// }>
+// // BaseResponse<{
+// //     item: Task;
+// // }>
 
-// BaseResponse<{
-//     item: Todolist;
-// }>
+// // BaseResponse<{
+// //     item: Todolist;
+// // }>
 
-export const handleServerAppError = <T>(data: BaseResponse<T>, dispatch: Dispatch) => {
-  dispatch(setError({ error: data.messages.length ? data.messages[0] : "some error occured" }))
-  dispatch(setStatus({ status: "failed" }))
-}
+// export const handleServerAppError = <T>(data: BaseResponse<T>, dispatch: Dispatch) => {
+//   dispatch(setError({ error: data.messages.length ? data.messages[0] : "some error occured" }))
+//   dispatch(setStatus({ status: "failed" }))
+// }
--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,29 +1,29 @@
-import { setError, setStatus } from "@/app/app-slice"
-import type { Dispatch } from "@reduxjs/toolkit"
-import axios from "axios"
-import { z } from "zod"
+// import { setError, setStatus } from "@/app/app-slice"
+// import type { Dispatch } from "@reduxjs/toolkit"
+// import axios from "axios"
+// import { z } from "zod"
 
-export const handleServerNetworkError = (error: unknown, dispatch: Dispatch) => {
-  let errorMessage = "some error occurred"
+// export const handleServerNetworkError = (error: unknown, dispatch: Dispatch) => {
+//   let errorMessage = "some error occurred"
 
-  switch (true) {
-    case axios.isAxiosError(error):
-      errorMessage = error.response?.data?.message || error.message
-      break
+//   switch (true) {
+//     case axios.isAxiosError(error):
+//       errorMessage = error.response?.data?.message || error.message
+//       break
 
-    case error instanceof z.ZodError:
-      console.table(error.issues)
-      errorMessage = "Zod error. Смотри консоль"
-      break
+//     case error instanceof z.ZodError:
+//       console.table(error.issues)
+//       errorMessage = "Zod error. Смотри консоль"
+//       break
 
-    case error instanceof Error:
-      errorMessage = `Native error: ${error.message}`
-      break
+//     case error instanceof Error:
+//       errorMessage = `Native error: ${error.message}`
+//       break
 
-    default:
-      errorMessage = JSON.stringify(error)
-  }
+//     default:
+//       errorMessage = JSON.stringify(error)
+//   }
 
-  dispatch(setError({ error: errorMessage }))
-  dispatch(setStatus({ status: "failed" }))
-}
+//   dispatch(setError({ error: errorMessage }))
+//   dispatch(setStatus({ status: "failed" }))
+// }
